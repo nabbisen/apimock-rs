26012dd7289f29fcb98f9a24992598cf0535758f
nabbisen <nabbisen@scqr.net>
2024-05-24 11:28:21 +0900

    fix release asset missed api.json5 in tests; update screenshots and dev-deps

--- .github/workflows/scripts/make-release-asset.sh
+++ .github/workflows/scripts/make-release-asset.sh
@@ -24,6 +24,7 @@ cd target/$target/release
 mkdir -p apimock-rs-main/tests
 cp $GITHUB_WORKSPACE/apimock.toml apimock-rs-main/
 cp $GITHUB_WORKSPACE/tests/home.json apimock-rs-main/tests/
+cp $GITHUB_WORKSPACE/tests/api.json5 apimock-rs-main/tests/
 os_tag=$3
 case $1 in
   ubuntu*)
