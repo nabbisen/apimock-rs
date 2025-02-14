1122663c5f598947079e2e9db8e115920d8044a3
nabbisen <nabbisen@scqr.net>
2025-02-15 00:23:56 +0900

    impl feature to be called as subprocess via spawn

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,19 @@ pub mod core;
 use core::app::App;
 
 /// return hyper http server
+#[cfg(not(feature = "spawn"))]
 pub async fn server(config_path: &str) -> App {
-    App::new(config_path).await
+    App::new(config_path, None).await
+}
+
+#[cfg(feature = "spawn")]
+use tokio::sync::mpsc::Sender;
+
+/// accept sender to main proc set to logger and
+/// return hyper http server
+#[cfg(feature = "spawn")]
+pub async fn server(config_path: &str, spawn_tx: Sender<String>) -> App {
+    App::new(config_path, Some(spawn_tx)).await
 }
 
 /// start hyper http server (deprecated)
