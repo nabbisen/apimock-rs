27ab9154729b9d96e9def95447764d80a4adfcdb
nabbisen <nabbisen@scqr.net>
2025-03-08 23:10:31 +0900

    omit ansi escape code for console color text when running as subprocess

--- src/core/logger.rs
+++ src/core/logger.rs
@@ -1,24 +1,32 @@
 use std::sync::OnceLock;
 
+use console::strip_ansi_codes;
 use log::{Level, Metadata, Record, SetLoggerError};
 use tokio::sync::mpsc::Sender;
 
+/// logger
+static LOGGER: OnceLock<AppLogger> = OnceLock::new();
+
+/// log output
 #[derive(Clone)]
 enum LogOutput {
     Stdout,
     Sender(Sender<String>),
 }
 
+/// app logger
 #[derive(Clone)]
 struct AppLogger {
     output: LogOutput,
 }
 
 impl log::Log for AppLogger {
+    /// check if it is enabled
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= Level::Info
     }
 
+    /// log print out
     fn log(&self, record: &Record) {
         if !self.enabled(record.metadata()) {
             return;
@@ -35,7 +43,8 @@ impl log::Log for AppLogger {
                 let msg = format!(
                     "[{}] {}",
                     record.level().to_string().chars().next().unwrap(),
-                    record.args()
+                    // omit ansi escape codes for console text color
+                    strip_ansi_codes(record.args().to_string().as_ref())
                 );
 
                 let tx = tx.clone();
@@ -46,19 +55,21 @@ impl log::Log for AppLogger {
         }
     }
 
+    /// flush
     fn flush(&self) {}
 }
 
-static LOGGER: OnceLock<AppLogger> = OnceLock::new();
-
+/// init logger
 pub fn init_logger(tx: Option<Sender<String>>) -> Result<(), SetLoggerError> {
     let output = if let Some(tx) = tx {
         LogOutput::Sender(tx)
     } else {
         LogOutput::Stdout
     };
+
     LOGGER.set(AppLogger { output }).ok();
     log::set_logger(LOGGER.get().unwrap())?;
     log::set_max_level(log::LevelFilter::Info);
+
     Ok(())
 }
