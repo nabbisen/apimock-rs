a606bbffdb5c0ed85e0dfefa5a92ab48437d664c
nabbisen <nabbisen@scqr.net>
2025-05-25 12:17:53 +0900

    fix #144 access to directory now results in 404 instead of 400

--- tests/server/routing/dyn_route/dir.rs
+++ tests/server/routing/dyn_route/dir.rs
@@ -1,12 +1,6 @@
 use hyper::StatusCode;
 
-use crate::{
-    constant::{CONFIG_TESTS_ROOT_DIR_PATH, DYN_ROUTE_DIR},
-    util::{
-        http::{http_response_default, response_body_str},
-        test_setup::TestSetup,
-    },
-};
+use crate::util::{http::http_response_default, test_setup::TestSetup};
 
 #[tokio::test]
 async fn dyn_data_dir_dir() {
@@ -15,14 +9,4 @@ async fn dyn_data_dir_dir() {
     let response = http_response_default("/html", port).await;
 
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
-
-    let body_str = response_body_str(response).await;
-    assert_eq!(
-        body_str.as_str(),
-        format!(
-            "{}/{}/html is not a file. must be missing or a directory",
-            CONFIG_TESTS_ROOT_DIR_PATH, DYN_ROUTE_DIR
-        )
-        .as_str()
-    );
 }
